02eea34099e768be3c3480ee60ae5e88c5103ad8
Benedict Cummins-Mburu <mburu.benedict@yahoo.com>
2026-06-07 14:12:01 -0400

    some small additions

--- scripts/data_summaries/02-donation_data_spatial.R
+++ scripts/data_summaries/02-donation_data_spatial.R
@@ -12,7 +12,9 @@
 library(tidyverse)
 library(lubridate)
 library(arrow)
-FED_OOD_data <- read_parquet("data/processed_data/FED_donations_data.parquet")
+FED_donations_data <- read_parquet(
+  "data/processed_data/FED_donations_data.parquet"
+)
 
 PARTIES_VISUALIZED <- c(
   "Conservative Party of Canada",
@@ -20,3 +22,20 @@ PARTIES_VISUALIZED <- c(
   "New Democratic Party",
   "Green Party of Canada"
 )
+
+FED_senders <- FED_donations_data %>%
+  group_by(sending_district) %>%
+  summarise(
+    n_donations_sent = sum(n_donations_all_both),
+    donation_amount_sent = sum(donation_amount_all_both)
+  )
+
+
+meann <- mean(FED_senders$donation_amount_sent)
+mediann <- median(FED_senders$donation_amount_sent)
+FED_senders %>%
+  ggplot(aes(x = donation_amount_sent)) +
+  geom_histogram() +
+  theme_classic() +
+  geom_vline(xintercept = meann, linetype = "dotted") +
+  geom_vline(xintercept = mediann, linetype = "solid")
